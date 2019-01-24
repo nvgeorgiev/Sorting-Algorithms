13c4400dac6670cb9950712f845125fb479ede84
Nikolay Georgiev <Nikolay.Georgiev@euromoneyplc.com>
2019-01-24 15:14:56 +0200

    Added Selection Sort to the main Algorithms

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SelectionSort
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentMinimum = numbers[i];
+
+                int currentMinimumIndex = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int element = numbers[j];
+
+                    if (element < currentMinimum)
+                    {
+                        currentMinimum = element;
+                        currentMinimumIndex = j;
+                    }
+                }
+
+                numbers[currentMinimumIndex] = numbers[i];
+                numbers[i] = currentMinimum;
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
