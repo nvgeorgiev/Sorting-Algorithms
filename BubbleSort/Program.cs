7a98359095c7817a1dcad47c13f22aadf68e20ac
Georgiev <Nikolay.Georgiev@euromoneyplc.com>
2019-01-24 11:56:52 +0200

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace BubbleSort
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48 };
+
+            bool swapped = true;
+
+            do
+            {
+                swapped = false;
+
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    int leftElement = numbers[i];
+                    int rightElement = numbers[i + 1];
+
+                    if (leftElement > rightElement)
+                    {
+                        numbers[i] = rightElement;
+                        numbers[i + 1] = leftElement;
+
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
